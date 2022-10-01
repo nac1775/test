2615da44fc82f2e6c6e8485da67d0ab6512d071c
alsru <alsru@DESKTOP-HFOKTGC>
2022-10-01 20:18:48 +0900

    test2.0

--- src/test1/test_java1.java
+++ src/test1/test_java1.java
@@ -4,6 +4,20 @@ public class test_java1 {
 
 	public static void main(String[] args) {
 			
+		int year = 0, age = 14;
+			year = age + 2000;
+			
+			age = age + 1;
+		int	name = age + year;
+		System.out.println(year);
+		
+		double pi = 3.14;
+		char ch ='가';
+		System.out.println(ch);
+		System.out.println(pi);
+		
+	final int NAX = 100 ;
+	
 	}
 
 }
